d42419ac10df68886dbafb370e9940017b6f43ab
Andy <avadnais@oswego.edu>
2022-08-18 22:28:42 -0600

    google OAuth

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const keys = require('./config/keys');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientId,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		// once auth is complete, do this
+		(accessToken, refreshToken, profile, done) => {
+			console.log('access token', accessToken);
+			console.log('refresh token', refreshToken)
+			console.log('profile', profile)
+		}
+	)
+);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: [ 'profile', 'email' ]
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
