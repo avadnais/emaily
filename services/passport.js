e389106c0a2241dd3ff03c93b7886df18d6a6424
Andy <avadnais@oswego.edu>
2022-08-21 13:44:35 -0600

    cookie

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+	done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user)
+	})
+})
 
 passport.use(
 	new GoogleStrategy(
@@ -11,9 +24,19 @@ passport.use(
 		},
 		// once auth is complete, do this
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile', profile);
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					// already a user with this id
+					done(null, existingUser);
+				} else {
+					// we don't have a user with this id yet
+					new User({
+						googleId: profile.id
+					})
+						.save()
+						.then((user) => done(null, user));
+				}
+			});
 		}
 	)
 );
