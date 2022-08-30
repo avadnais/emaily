1669a7ed8cb4727bd9d5d8fe028efdffecc10c54
Andy <avadnais@oswego.edu>
2022-08-30 17:34:06 -0600

    payments component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
 	renderContent() {
@@ -14,11 +15,14 @@ class Header extends Component {
 					</li>
 				);
 			default:
-				return (
+				return [
+					<li>
+						<Payments />
+					</li>,
 					<li>
 						<a href="/api/logout">Logout</a>
 					</li>
-				);
+				]
 		}
 	}
 
@@ -26,7 +30,7 @@ class Header extends Component {
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<Link to={this.props.auth ? "/surveys" : "/"} className="left brand-logo" >
+					<Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
 						Emaily
 					</Link>
 					<ul className="right">{this.renderContent()}</ul>
