dae308586fe6d90b47e5a015df4ac901fb859bdc
Andy <avadnais@oswego.edu>
2022-09-11 19:49:31 -0600

    defined survey schema and started survey route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
@@ -23,7 +24,8 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app); // ./routes/authRoutes returns a function then invokes immediately with (app)
 require('./routes/billingRoutes')(app);
-//
+require('./routes/surveyRoutes')(app);
+
 if (process.env.NODE_ENV === 'production') {
 	// express will serve production assets like main.js or main.css
 	app.use(express.static('client/build'));
